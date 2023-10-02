ee91ea3c39fd4d7161199dd4152e3be9ac206a9e
Justin Tham <justinctham@hotmail.com>
2023-10-02 16:06:00 -0700

    added files

---
+++ app/src/components/TOS.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+export default function TOS() {
+    return (
+        <main>
+            <div class="image">
+                <img src="images/allyoucaneat.png" alt="Food" style={{ width: '100%', height: 'auto' }} />
+                <div class="top-text">
+                    ALL YOU CAN EAT
+                    <div class="text-center">
+                        GUIDELINES
+                    </div>
+                </div>
+            </div>
+            <div class="container-fluid terms">
+                <p class="heading">Does every party member get charged AYCE?</p>
+                <p>Due to the frequent incidents.. tables who order the all you can eat option, every single guest will be charged for the all you can eat price (no exceptions). Those who would not like to eat or are vegetarians will also be charged if they are sitting at the table**</p>
+                <p class="heading">Is free rice available for AYCE customers?</p>
+                <p>Please let our staff know you would like rice. We have seen an increase amount of food waste for NOT eating your rice. To reduce food waste we will no longer provide rice unless asked.</p>
+                <p class="heading">When does the AYCE close?</p>
+                <p>For the best experience (1.5 hours), we will take AYCE customers up to 1.5 hour prior to closing" All customers coming in after will be offered our a la carte, ready to eat, and combos selections.</p>
+                <p class="heading">GENERAL RULES:</p>
+                <p>
+                    Time limit 1.5 hrs <br />
+                    Everyone at the table must order AYCE <br />
+                    Minimum of 2 AYCE orders <br />
+                    Must finish all plates before ordering more <br />
+                    Only 4 items can be ordered at a time <br />
+                    Take out is not allowed <br />
+                    Extra charge of <b>$1.50 for each rice bowl or soup left over</b> <br />
+                    Extra charge of <b>$5 per person for any meat leftover</b> <br />
+                    Food can not be taken to-go <br />
+                    **Friday-Sunday We look forward to serving your party and ensuring you have a great time and experience at Baegopa! Due to the limited seating capacity, we will need to limit the experience to 2 hours per table so we may allow everyone an equal opportunity to enjoy their time at Baegopa.**<br />
+                    <br />
+                    Thank you for your understanding.
+                </p>
+
+            </div>
+        </main>
+    )
+};
