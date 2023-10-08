6cf1b2f96d66a198a9ff4532ec6eeb53c5010002
Justin Tham <justinctham@hotmail.com>
2023-10-07 23:49:16 -0700

    added schema

---
+++ app/sanity-studio/schemas/appetizers.js
@@ -0,0 +1,22 @@
+export default {
+    name: 'appetizers',
+    type: 'document',
+    title: 'Appetizers',
+    fields: [
+        {
+            name: 'korean',
+            type: 'string',
+            title: 'Korean'
+        },
+        {
+            name: 'english',
+            type: 'string',
+            title: 'English'
+        },
+        {
+            name: 'price',
+            type: 'string',
+            title: 'Price'
+        }
+    ]
+}
--- app/sanity-studio/schemas/index.js
+++ app/sanity-studio/schemas/index.js
@@ -1,1 +1,3 @@
-export const schemaTypes = []
+import appetizers from './appetizers'
+
+export const schemaTypes = [appetizers]
